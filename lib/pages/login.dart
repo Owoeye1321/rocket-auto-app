315c3cd9d817f098d70fa84449fd948b075b1005
Owoeye Samuel <owoeye1321@gmail.com>
2024-07-26 21:00:08 +0100

    oh lord dam! ⚒

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,10 +1,62 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
-class Login extends StatelessWidget {
+import '../utilities/colors.dart';
+
+class Login extends StatefulWidget {
   const Login({super.key});
 
   @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        body: SizedBox(
+      width: double.infinity,
+      child: Form(
+        child: Container(
+          margin: EdgeInsets.symmetric(
+            horizontal: 60,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: EdgeInsets.symmetric(
+                  vertical: 20,
+                ),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "******@gmail.com",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(
+                          100.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: rootPink,
+                  //minimumSize: Size(200, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+                onPressed: () {},
+                child: Text("Submit"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ));
   }
 }
